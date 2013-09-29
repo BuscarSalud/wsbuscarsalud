44136444525e962f9d5823d0476b5b929f11a1fb
Felix <felixoe@gmail.com>
2013-09-29 18:59:07 -0500

    modified

--- get_privacy_policy.php
+++ get_privacy_policy.php
@@ -5,13 +5,6 @@ header("Cache-Control: no-cache, must-revalidate");
 header("Content-type: application/json");
 header('Content-Type: text/html; charset=UTF-8');
 
-// START BOOTSTRAP DRUPAL
-define('DRUPAL_ROOT', '/var/www/dev.buscarsalud/html');
-$_SERVER['REMOTE_ADDR'] = "localhost"; // Necessary if running from command line
-require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-// END BOOTSTRAP DRUPAL
-
 $node = node_load(2318990);
 
 $terms = $node->body['und']['0']['safe_value'];
