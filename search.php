3a5db62f7f8a1309a9a82b9473981c2ec585661e
Cristian Cienfuegos <cristian100@gmail.com>
2013-12-13 09:42:18 -0700

    added search script

---
+++ search.php
@@ -0,0 +1,24 @@
+<?php
+include_once('bootstrap.inc');
+header("Content-Type: application/json");
+header("Expires: on, 01 Jan 1970 00:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+if( isset($_GET['palabras']) ){
+	//$params['palabras'] = $_GET['palabras'];
+}
+
+$params['limit'] = 10;
+$params['page'] = 1;
+
+$results = buscarsalud_data_get_profiles($params);
+buscarsalud_data_prepare_profiles($results, 'http://www.buscarsalud.com');
+
+//echo json_encode($results);
+//print_r($params);
+exit;
+
+?>
